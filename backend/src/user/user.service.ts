91c3b2804b3631a667d939ddcbb4d4c5924640a4
Eduardo Fernandes <edufernandes.1895@gmail.com>
2025-12-08 23:42:51 -0300

    feat(user): add UUID generation for user creation and update .gitignore for SQLite files

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { v7 as uuidv7 } from 'uuid';
 
 @Injectable()
 export class UserService {
@@ -12,7 +13,7 @@ export class UserService {
   ) {}
 
   create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create(createUserDto);
+    const user = this.userRepository.create({ ...createUserDto, id: uuidv7() });
     return this.userRepository.save(user);
   }
 
